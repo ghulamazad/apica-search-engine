02108bcc6f61a472f78bf326da731fcc75413470
Md.Ghulam Azad Ansari <iamghulamazad@gmail.com>
2025-04-27 09:46:04 +0530

    feat: Implement file upload handling and remove deprecated file loading logic

--- backend/api/handlers.go
+++ backend/api/handlers.go
@@ -1,15 +1,32 @@
 package api
 
 import (
+	"context"
 	"encoding/json"
+	"fmt"
+	"log"
 	"net/http"
 	"time"
 
+	"github.com/ghulamazad/GFileMux"
+	"github.com/ghulamazad/GFileMux/storage"
 	"github.com/ghulamazad/apica-search-engine/indexer"
 )
 
 type SearchHandler struct {
 	Indexer *indexer.InvertedIndex
+	Disk    *storage.DiskStorage
+}
+
+func (h *SearchHandler) LoadFiles(files []string) {
+	for _, file := range files {
+		records, err := indexer.ParseParquet(file)
+		if err != nil {
+			log.Fatalf("Error parsing file %s: %v", file, err)
+		}
+
+		h.Indexer.AddRecords(records)
+	}
 }
 
 func (h *SearchHandler) Search(w http.ResponseWriter, r *http.Request) {
@@ -24,4 +41,40 @@ func (h *SearchHandler) Search(w http.ResponseWriter, r *http.Request) {
 		"count":    len(results),
 		"duration": duration.String(),
 	})
+}
+
+func (h *SearchHandler) UploadHandler(w http.ResponseWriter, r *http.Request) {
+	// Retrieve the uploaded files from the request context
+	files, err := GFileMux.GetUploadedFilesFromContext(r)
+	if err != nil {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(fmt.Sprintf(`{"error": "%s"}`, fmt.Sprintf("Failed to get uploaded files: %v", err))))
+		return
+	}
+
+	var uploadedFiles []string
+	// Process each uploaded file and print details
+	for _, file := range files {
+		// Print the file path in disk storage
+		filePath, err := h.Disk.Path(context.Background(), GFileMux.PathOptions{
+			Key:    file[0].StorageKey,
+			Bucket: file[0].FolderDestination,
+		})
+		if err != nil {
+			log.Printf("Error retrieving file path for %s: %v", file[0].StorageKey, err)
+			continue // Skip to the next file if there's an error
+		}
+
+		uploadedFiles = append(uploadedFiles, filePath)
+	}
+
+	// load the uploaded files
+	h.LoadFiles(uploadedFiles)
+
+	// Respond with a success message
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(`{"message": "Files uploaded successfully"}`))
+
 }
--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"log"
 	"net/http"
@@ -15,17 +14,6 @@ import (
 	"github.com/gorilla/mux"
 	"github.com/rs/cors"
 )
-
-func loadFiles(idx *indexer.InvertedIndex, files []string) {
-	for _, file := range files {
-		records, err := indexer.ParseParquet(file)
-		if err != nil {
-			log.Fatalf("Error parsing file %s: %v", file, err)
-		}
-
-		idx.AddRecords(records)
-	}
-}
 
 func getGFileMuxMiddleware() (*GFileMux.GFileMux, *storage.DiskStorage, error) {
 	// Initialize disk storage
@@ -66,25 +54,6 @@ func getGFileMuxMiddleware() (*GFileMux.GFileMux, *storage.DiskStorage, error) {
 func main() {
 	idx := indexer.NewInvertedIndex()
 
-	files := []string{
-		"data/File 1",
-		"data/File 2",
-		"data/File 3",
-		"data/File 4",
-		"data/File 5",
-		"data/File 6",
-		"data/File 7",
-		"data/File 8",
-		"data/File 9",
-		"data/File 10",
-		"data/File 11",
-		"data/File 14",
-		"data/File 15",
-		"data/File 16",
-	}
-
-	loadFiles(idx, files)
-
 	r := mux.NewRouter()
 
 	// File upload middleware
@@ -93,15 +62,20 @@ func main() {
 		log.Fatalf("Error initializing GFileMux: %v", err)
 	}
 
-	// Search handler
 	h := &api.SearchHandler{
 		Indexer: idx,
+		Disk:    storageDisk,
 	}
+
+	// load initial files
+	h.LoadFiles([]string{"data/File 1", "data/File 2"})
+
+	// Search handler
 	r.HandleFunc("/search", h.Search).Methods("GET")
 
 	// Upload handler
 	r.HandleFunc("/upload", func(w http.ResponseWriter, r *http.Request) {
-		gFileMux.Upload("bucket_name", "files")(http.HandlerFunc(uploadHandler(storageDisk, idx))).ServeHTTP(w, r)
+		gFileMux.Upload("bucket_name", "files")(http.HandlerFunc(h.UploadHandler)).ServeHTTP(w, r)
 	}).Methods("POST")
 
 	// Wrap router with CORS middleware
@@ -109,45 +83,4 @@ func main() {
 
 	log.Println("Server running on :8080")
 	log.Fatal(http.ListenAndServe(":8080", handler))
-}
-
-func respondWithError(w http.ResponseWriter, statusCode int, message string) {
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(statusCode)
-	w.Write([]byte(fmt.Sprintf(`{"error": "%s"}`, message)))
-}
-
-func uploadHandler(disk *storage.DiskStorage, idx *indexer.InvertedIndex) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		// Retrieve the uploaded files from the request context
-		files, err := GFileMux.GetUploadedFilesFromContext(r)
-		if err != nil {
-			respondWithError(w, http.StatusInternalServerError, fmt.Sprintf("Failed to get uploaded files: %v", err))
-			return
-		}
-
-		var uploadedFiles []string
-		// Process each uploaded file and print details
-		for _, file := range files {
-			// Print the file path in disk storage
-			filePath, err := disk.Path(context.Background(), GFileMux.PathOptions{
-				Key:    file[0].StorageKey,
-				Bucket: file[0].FolderDestination,
-			})
-			if err != nil {
-				log.Printf("Error retrieving file path for %s: %v", file[0].StorageKey, err)
-				continue // Skip to the next file if there's an error
-			}
-
-			uploadedFiles = append(uploadedFiles, filePath)
-		}
-
-		// load the uploaded files
-		loadFiles(idx, uploadedFiles)
-
-		// Respond with a success message
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte(`{"message": "Files uploaded successfully"}`))
-	}
 }
